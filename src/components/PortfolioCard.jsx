3bb8ea5fc52f2022380fbd9e174521876a6236b1
unknown <dev.rafaalrazzak@gmail.com>
2023-01-24 23:38:18 +0700

    Portfolio Card Parallax

--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -4,6 +4,7 @@ import Balancer from "react-wrap-balancer"
 import DynamicIcon from "@/components/DynamicIcon"
 import Link from "@/components/Link"
 import Tooltip from "@/components/Tooltip"
+import Image from "@/components/Image"
 
 export default function PortfolioCard({
   title,
@@ -14,14 +15,18 @@ export default function PortfolioCard({
 }) {
   return (
     <div
-      className="fixed relative sticky flex  h-64 flex-col justify-between overflow-clip rounded-lg bg-cover  bg-center px-4 py-2 text-white  transition-all duration-300 ease-in-out before:absolute before:inset-0 before:bg-gradient-to-t before:from-black after:to-transparent"
-      style={{
-        backgroundImage: `url(${image})`,
-      }}
+      className="fixed top-24 relative sticky flex  h-64 w-full flex-col justify-between overflow-clip rounded-xl bg-cover  bg-center px-4 py-2 text-white  transition-all duration-300 ease-in-out after:bg-gradient-to-t  after:from-black after:to-transparent after:absolute after:inset-0 after:z-2"
     >
-      <div className="flex items-center justify-between ">
+      <Image
+        src={image}
+        alt={title}
+        className="absolute rounded-xl inset-0 w-full h-full object-cover object-center after:bg-gradient-to-t  after:from-black after:via-black after:to-transparent after:absolute after:inset-0 after:z-10"
+        fill
+        sizes="(max-width: 800px) 100vw, (max-width: 1200px) 50vw, 40vw"
+      />
+      <div className="flex items-center justify-between z-10">
         {stack && (
-          <div className="inline-flex gap-3 rounded-full bg-black/20 px-2 py-1 backdrop-blur-xl">
+          <div className="inline-flex gap-3 rounded-full bg-black/40 px-2 py-1 backdrop-blur-xl">
             {stack.map((tool) => (
               <Tooltip key={tool} title={tool} position="bottom">
                 <DynamicIcon name={tool} transform="scale(0.7)" />
@@ -32,12 +37,12 @@ export default function PortfolioCard({
 
         <Link
           href={link}
-          className="absolute top-2 right-3 inline-flex cursor-pointer rounded-full bg-black/20 p-2 backdrop-blur-xl"
+          className="absolute top-2 right-3 inline-flex cursor-pointer rounded-full bg-black/40 p-2 backdrop-blur-xl"
         >
           <ArrowTopRightOnSquareIcon className="h-4 w-4 hover:text-teal-400" />
         </Link>
       </div>
-      <div className="absolute bottom-4 flex flex-col px-2">
+      <div className="absolute bottom-4 flex flex-col px-2 flex-1 z-10  w-full pr-8">
         <Balancer className="font-bold text-white">{title}</Balancer>
         <Balancer className="text-xs leading-relaxed text-primary-400 line-clamp-2">
           {description}
--- src/components/Spotify/NowPlaying.jsx
+++ src/components/Spotify/NowPlaying.jsx
@@ -1,80 +1,77 @@
-import { usePalette } from "color-thief-react"
+import {usePalette} from "color-thief-react"
 
 import Image from "@/components/Image"
-import { SpotifyIcon } from "@/icons"
+import {SpotifyIcon} from "@/icons"
 
 import Link from "../Link"
 
-export default function NowPlaying({ songUrl, songImage, title, artist }) {
-  const { data: dominantColor } = usePalette(songImage, 2, "hex", {
-    crossOrigin: "RAF",
-  })
+export default function NowPlaying({songUrl, songImage, title, artist}){
+	const {data: dominantColor} = usePalette(songImage, 2, "hex", {
+		crossOrigin: "RAF",
+	})
 
-  return (
-    <div className="group/nowPlaying relative flex items-center overflow-hidden rounded-xl">
-      <div className="flex w-full flex-1 gap-3">
-        <div
-          className="relative flex w-full flex-1 justify-between"
-          style={{
-            background: dominantColor
-              ? dominantColor[0]
-              : "rgba(255, 255, 255, 0.1)",
-          }}
-        >
-          <div
-            className="p-4"
-            style={{ color: dominantColor ? dominantColor[1] : "black" }}
-          >
-            <div className="inline-flex items-center gap-1 text-xs">
-              <SpotifyIcon
-                className="scale-[0.7]"
-                fill={dominantColor ? dominantColor[1] : "black"}
-              />
-              <p className="font-bold">Now Playing</p>
-            </div>
-            <div className="flex flex-1 flex-col gap-2">
-              <h3 style={{ color: dominantColor ? dominantColor[1] : "black" }}>
-                {title}
-              </h3>
-              <h4
-                className="text-xs font-medium"
-                style={{ color: dominantColor ? dominantColor[1] : "black" }}
-              >
-                {artist}
-              </h4>
-            </div>
-          </div>
-          <div className="relative w-1/2 rounded-r-lg md:w-40">
-            <Image
-              src={songImage}
-              alt={title}
-              className="aspect-square bg-contain bg-center object-cover object-center"
-              fill
-              sizes="(max-width: 800px) 100vw, (max-width: 1200px) 50vw, 40vw"
-            />
-            <div
-              className="absolute inset-0 -ml-1"
-              style={{
-                background: `linear-gradient(90deg, ${
-                  dominantColor ? dominantColor[0] : "#00000"
-                } 0%, rgba(255, 255, 255, 0) 100%)`,
-              }}
-            ></div>
-            <Link
-              href={songUrl}
-              className="absolute inset-0 z-10 flex h-full items-end justify-end p-2 opacity-0 transition-opacity duration-300 group-hover/nowPlaying:opacity-100"
-              rel="noopener noreferrer"
-            >
-              <div className="flex items-center justify-end gap-1 rounded-full bg-black/30 p-1 backdrop-blur-xl">
-                <SpotifyIcon className="scale-[0.7]" />
-                <p className="text-xs font-medium text-gray-400">
-                  Open in Spotify
-                </p>
-              </div>
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+	return (
+			<div className="group/nowPlaying relative flex items-center overflow-clip  rounded-xl">
+
+				<div className="flex w-full flex-1 gap-3">
+					<div
+							className="relative flex w-full flex-1 justify-between rounded-xl"
+							style={{
+								background: dominantColor ? dominantColor[0] : "rgba(255, 255, 255, 0.1)",
+							}}
+					>
+						<div
+								className="p-4"
+								style={{color: dominantColor ? dominantColor[1] : "black"}}
+						>
+							<div className="inline-flex items-center gap-1 text-xs">
+								<SpotifyIcon
+										className="scale-[0.7]"
+										fill={dominantColor ? dominantColor[1] : "black"}
+								/>
+								<p className="font-bold">Now Playing</p>
+							</div>
+							<div className="flex flex-1 flex-col gap-2">
+								<h3 style={{color: dominantColor ? dominantColor[1] : "black"}}>
+									{title}
+								</h3>
+								<h4
+										className="text-xs font-medium"
+										style={{color: dominantColor ? dominantColor[1] : "black"}}
+								>
+									{artist}
+								</h4>
+							</div>
+						</div>
+						<div className="relative w-1/2 rounded-r-lg md:w-40">
+							<Image
+									src={songImage}
+									alt={title}
+									className="aspect-square bg-contain bg-center object-cover object-center"
+									fill
+									sizes="(max-width: 800px) 100vw, (max-width: 1200px) 50vw, 40vw"
+							/>
+							<div
+									className="absolute inset-0 -ml-1"
+									style={{
+										background: `linear-gradient(90deg, ${dominantColor ? dominantColor[0] : "#00000"} 0%, rgba(255, 255, 255, 0) 100%)`,
+									}}
+							></div>
+							<Link
+									href={songUrl}
+									className="absolute inset-0 z-10 flex h-full items-end justify-end p-2 opacity-0 transition-opacity duration-300 group-hover/nowPlaying:opacity-100"
+									rel="noopener noreferrer"
+							>
+								<div className="flex items-center justify-end gap-1 rounded-full bg-black/30 p-1 backdrop-blur-xl">
+
+									<SpotifyIcon className="scale-[0.7]"/>
+									<p className="text-xs font-medium text-gray-400 line-clamp-1">
+										Open in Spotify
+									</p>
+								</div>
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>)
 }
