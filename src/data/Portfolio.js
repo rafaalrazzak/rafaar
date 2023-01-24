48d74d8d024417184aa8fb10a15820193c2eab88
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2023-01-24 12:47:52 +0000

    Project Image

--- src/data/Portfolio.js
+++ src/data/Portfolio.js
@@ -4,7 +4,7 @@ const Portfolio = [
     title: "Personal Website",
     description: "A personal website built with NextJs, MDX, TailwindCSS.",
     image:
-      "https://api.cloudinary.com/v1_1/raf-ar/image/download?api_key=762939459481927&format=jpg&public_id=portfolio%2Frafaar.me&signature=8f989e8cb94995f631942da6eb1c21f35bcec512&timestamp=1674478059&transformation=c_pad%2Cb_auto%3Apredominant%2Cfl_preserve_transparency&type=upload",
+      "https://res.cloudinary.com/raf-ar/image/upload/v1674477790/portfolio/rafaar.me.png",
     link: "https://rafaar.me",
     stack: ["NextJs", "TailwindCSS", "Vercel"],
   },
@@ -14,7 +14,7 @@ const Portfolio = [
     description:
       "A website for OSIS/MPK SMK AL-ASIYAH built with NextJs, TailwindCSS, Supabase.",
     image:
-      "https://api.cloudinary.com/v1_1/raf-ar/image/download?api_key=762939459481927&format=jpg&public_id=portfolio%2Fosis.matick.live&signature=34ef2bc3edee6b890aec233387a9a6f7725a59ce&timestamp=1674478086&transformation=c_pad%2Cb_auto%3Apredominant%2Cfl_preserve_transparency&type=upload",
+      "https://res.cloudinary.com/raf-ar/image/upload/v1674477790/portfolio/osis.matick.live.png",
     link: "https://osis.matick.live",
     stack: ["NextJs", "TailwindCSS", "Supabase", "Vercel"],
   },
