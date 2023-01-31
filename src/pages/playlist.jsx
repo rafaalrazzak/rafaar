dc5f80b42b851db5327e4e58d3a3261fd9fcd994
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2023-01-31 17:05:24 +0700

    Playlist Pages

--- src/pages/playlist.jsx
+++ src/pages/playlist.jsx
@@ -1,63 +1,18 @@
-import { MagnifyingGlassIcon, PlusSmallIcon } from "@heroicons/react/24/outline"
-import Image from "next/image"
-import { useState } from "react"
+import {MagnifyingGlassIcon} from "@heroicons/react/24/outline"
+import {useState} from "react"
 
-import { SEO } from "@/components/SEO"
-import { DefaultLayout } from "@/layout"
+import {SEO} from "@/components/SEO"
+import AddSong from "@/components/Spotify/AddSong"
+import {DefaultLayout} from "@/layout"
 
-const API_URL = "https://api.rafaar.me/api/v1/spotify/"
 
-function Song({ title, artist, songImage, songUri }) {
-  const handleAdd = async (e) => {
-    e.preventDefault()
-    const response = await fetch(
-      `${API_URL}add-to-playlist?songUri=${songUri}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    ).then((res) => res.json())
-    alert(response?.message)
-  }
-
-  return (
-    <div className="flex w-full items-center justify-between gap-2 rounded-md bg-primary-50 p-3 dark:bg-primary-700">
-      <div className="flex gap-2">
-        <Image
-          src={songImage}
-          alt={title}
-          className="h-12 w-12 rounded-lg"
-          width={50}
-          height={50}
-        />
-        <div>
-          <h5>{title}</h5>
-          <p>{artist}</p>
-        </div>
-      </div>
-
-      <button
-        type={"button"}
-        onClick={handleAdd}
-        className="flex h-8 w-8 items-center justify-center rounded-full bg-green-500"
-      >
-        <PlusSmallIcon className="h-6 text-white" />
-      </button>
-    </div>
-  )
-}
-
-export default function Playlist() {
+export default function Playlist(){
   const [songSearch, setSongSearch] = useState("")
   const [results, setResults] = useState([])
 
-  const handleSearch = async (e) => {
+  const handleSearch = async(e) => {
     e.preventDefault()
-    setResults(
-      await fetch(`${API_URL}search?q=${songSearch}`).then((res) => res.json())
-    )
+    setResults(await fetch(`${process.env.NEXT_PUBLIC_API_URL}search?q=${songSearch}`).then((res) => res.json()))
   }
 
   const handleChange = (e) => {
@@ -65,50 +20,54 @@ export default function Playlist() {
   }
 
   return (
-    <DefaultLayout className="flex flex-col gap-4">
-      <SEO title="Playlist" />
-      <div className="flex flex-col  gap-4 lg:my-24">
-        <h1>Made for Me</h1>
-        <iframe
-          title="Made for Me"
-          src="https://open.spotify.com/embed/playlist/5R5IdlSxHI3a5aTRTSYyUr?&theme=white"
-          width="100%"
-          height={352}
-          frameborder={0}
-          allowFullScreen
-          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-          loading="lazy"
-          className="rounded-2xl"
-        />
-      </div>
-      <div className="flex flex-col gap-4">
-        <h3>Add song to the playlist</h3>
-        <form className="flex flex-col gap-2 rounded-xl bg-primary-100 p-6 dark:bg-primary-800">
-          <div className="flex items-center gap-2">
-            <input
-              onChange={handleChange}
-              type="search"
-              placeholder="Search song"
-              className="flex w-48 flex-1 rounded-full bg-transparent px-3 py-2 transition-colors duration-300 ease-linear focus:bg-green-500 focus:text-white focus:outline-none focus:placeholder:text-white md:w-auto"
-            />
-            <button
-              type={"submit"}
-              onClick={handleSearch}
-              className="flex rounded-full bg-green-500 p-3"
-            >
-              <MagnifyingGlassIcon className="h-4 text-white" />
-            </button>
-          </div>
-
-          {results.length > 0 && (
-            <div className="my-4 flex flex-col gap-4 lg:flex-row lg:flex-wrap">
-              {results.map((result, idx) => (
-                <Song key={idx} {...result} />
-              ))}
+      <DefaultLayout className="flex flex-col gap-4 min-h-screen">
+        <SEO title="Playlist"/>
+        <div className="flex flex-col  gap-4 md:my-24">
+          <h1>Made for Me</h1>
+          <iframe
+              title="Made for Me"
+              src="https://open.spotify.com/embed/playlist/5R5IdlSxHI3a5aTRTSYyUr?&theme=white"
+              width="100%"
+              height={352}
+              frameBorder={0}
+              allowFullScreen
+              allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+              loading="lazy"
+              className="rounded-2xl"
+          />
+        </div>
+        <div className="flex flex-col gap-4">
+          <h3>Add song to the playlist</h3>
+          <form>
+            <div className="flex items-center gap-2">
+              <input
+                  onChange={handleChange}
+                  type="search"
+                  placeholder="Search song"
+                  className="flex w-48 flex-1 rounded-full bg-transparent px-3 py-2 transition-colors duration-300 ease-linear focus:bg-green-500 hover:bg-green-500 hover:text-white ring-1 ring-green-500 focus:text-white focus:outline-none focus:placeholder:text-white md:w-auto"
+              />
+              <button
+                  type={"submit"}
+                  onClick={handleSearch}
+                  className="flex rounded-full transition-colors  duration-300 ease-linear focus:bg-green-500 hover:bg-green-500 ring-1 ring-green-500 dark:ring-green-500 p-3"
+              >
+                <MagnifyingGlassIcon className="h-4 text-primary-800 dark:text-white"/>
+              </button>
             </div>
+          </form>
+
+
+          {results?.length > 0 && (
+              <div className="flex flex-col gap-2 rounded-xl bg-primary-100 p-6 dark:bg-primary-800">
+                <div className="my-4 flex flex-col gap-4 lg:flex-row lg:flex-wrap">
+                  {results.map((result, idx) => (
+                      <AddSong key={idx} {...result} />
+                  ))}
+                </div>
+              </div>
           )}
-        </form>
-      </div>
+          
+        </div>
     </DefaultLayout>
   )
 }
--- src/pages/welcome.jsx
+++
@@ -1,3 +0,0 @@
-export default function Welcome() {
-  return <h1>hELLO</h1>
-}
