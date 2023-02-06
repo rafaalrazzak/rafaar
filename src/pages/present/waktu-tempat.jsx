4ddc3f8985ce70bb7eeead2f1b13443e45e52a8a
Rafa Al Razzak <94893946+rafaalrazzak@users.noreply.github.com>
2023-02-06 09:01:01 +0700

    Update waktu-tempat.jsx

--- src/pages/present/waktu-tempat.jsx
+++ src/pages/present/waktu-tempat.jsx
@@ -1,5 +1,3 @@
-import * as HeroIcons from "@heroicons/react/24/outline"
-
 import { SectionCard } from "@/components/Section"
 import TableOfContents from "@/data/ppt/TableOfContents"
 export default function WaktuTempat() {
