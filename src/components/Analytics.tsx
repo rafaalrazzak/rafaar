258e52ebe89dd4b4eb893711a3e6e83ec3bbfd39
Rafa Al Razzak <94893946+rafaalrazzak@users.noreply.github.com>
2023-12-04 14:08:25 +0700

    add Google Analytics

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -1,14 +1,29 @@
 import Script from 'next/script'
 
 const Analytics = () => {
-  
-    return (
+
+  return (
+    <>
       <Script
         async
-        src="https://eu.umami.is/script.js" 
+        src="https://eu.umami.is/script.js"
         data-website-id="452bc90f-47ab-4d3e-840e-fcb04b7d548f"
       />
-    )
-  }
-  
-  export default Analytics
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-WZEKV3JVLM"
+      />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          
+          gtag('config', 'G-WZEKV3JVLM');
+        `}
+      </Script>
+    </>
+  )
+}
+
+export default Analytics
