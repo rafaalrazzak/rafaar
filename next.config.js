2d92f23a257a8aac8ec8c8fc325d166c87bcc020
Rafa Al Razzak <94893946+rafaalrazzak@users.noreply.github.com>
2024-02-26 12:47:17 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     reactStrictMode: true,
     images: {
-        domains: ["res.cloudinary.com", "images.unsplash.com", "og-image.vercel.app", "i.scdn.co"],
+        domains: ["res.cloudinary.com", "images.unsplash.com", "og-image.vercel.app", "i.scdn.co", "cdn.kita.blue"],
     },
 };
 module.exports = nextConfig;
