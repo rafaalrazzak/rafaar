88249cf10b7a6e4974b69f319ba458dd658ac1d6
Rafa Al Razzak <dev.rafaalrazzak@gmail.com>
2024-05-26 20:09:47 +0700

    Refactor BentoCard and Navbar components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,15 +16,15 @@ export default function Navbar() {
     <header className='fixed left-0 right-0 top-0 z-[100] flex items-center justify-center py-2 print:hidden'>
       <nav className='flex gap-3 rounded-full border border-primary-700/80 bg-primary-800/50 px-4 backdrop-blur-xl'>
         {APP_ROUTE.map((item, i) => (
-          <Link key={i} href={item.path}>
-            <Button
-              variant='link'
-              className={cn(
-                `px-0 text-primary-300 ${isCurrentPath(item.path) ? 'font-bold text-primary-100' : 'text-primary-400'}`
-              )}
-            >
-              {item.name}
-            </Button>
+          <Link
+            variant='link'
+            key={i}
+            href={item.path}
+            className={cn(
+              `px-0 text-primary-300 ${isCurrentPath(item.path) ? 'font-bold text-primary-100' : 'text-primary-400'}`
+            )}
+          >
+            {item.name}
           </Link>
         ))}
       </nav>
